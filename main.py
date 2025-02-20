374a7e201aa5e75b9577c497a3cf3c3939eb634f
Andrew Ryan <andrrvt15@gmail.com>
2025-02-20 10:38:30 -0500

    adding player and circle shape

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import pygame
 from constants import *
+from circleshape import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -13,6 +16,7 @@ def main():
         screen.fill("black")
         pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
